e531488af15a69c5a5127a27b85f6b8e611ced3b
xjr1022 <xjr1022@gmail.com>
2019-03-16 06:04:32 +0800

    version 1.0

---
+++ client/main/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+//定义两个变量，一个表示用户id，一个表示用户密码
+var (
+	userId  int
+	userPwd string
+)
+
+func main() {
+	//接收用户的选择
+	var key int
+	//判断是继续显示主菜单
+	var loop = true
+
+	for loop {
+		fmt.Println("----------------欢迎登陆多人聊天系统------------")
+		fmt.Println("\t\t\t 1 登陆聊天室")
+		fmt.Println("\t\t\t 2 注册用户")
+		fmt.Println("\t\t\t 3 退出系统")
+		fmt.Println("\t\t\t 请选择(1-3):")
+
+		fmt.Scanf("%d\n", &key)
+		switch key {
+		case 1:
+			fmt.Println("登陆聊天室")
+			fmt.Println("请输入用户的id")
+			fmt.Scanf("%d\n", &userId)
+			fmt.Println("请输入用户的密码")
+			fmt.Scanf("%s\n", &userPwd)
+			err := Login(userId, userPwd)
+			if err != nil {
+				fmt.Println("登录失败")
+			} else {
+				fmt.Println("登录成功")
+			}
+		case 2:
+			fmt.Println("注册用户")
+			fmt.Println("请输入用户id:")
+			fmt.Scanf("%d\n", &userId)
+			fmt.Println("请输入用户密码:")
+			fmt.Scanf("%s\n", &userPwd)
+
+			//2. 调用UserProcess，完成注册的请求、
+
+		case 3:
+			fmt.Println("退出系统")
+			//loop = false
+			os.Exit(0)
+		default:
+			fmt.Println("你的输入有误，请重新输入")
+		}
+	}
+}
---
+++ common/message/message.go
@@ -0,0 +1,22 @@
+package message
+
+const (
+	LoginMesType    = "LoginMes"
+	LoginResMesType = "LoginResMes"
+)
+
+type Message struct {
+	Type string `json:"type"`
+	Data string `json:"data"`
+}
+
+//定义消息结构体
+type LoginMes struct {
+	UserId   int    `json:"userid"`   //用户id
+	UserPwd  string `json:"userPwd"`  //用户密码
+	UserName string `json:"userName"` //用户名
+}
+type LoginResMes struct {
+	Code  int    `json:"code"`  //返回状态码
+	Error string `json:"error"` //返回错误信息
+}
